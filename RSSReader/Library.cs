11aa520f71365df2fdb8872d5da83ca4d35a2fba
yamaguchiICT <yamaguchi@ict.ehime-u.ac.jp>
2017-03-27 09:39:11 +0900

    Library.cs coding completed ...

--- RSSReader/Library.cs
+++ RSSReader/Library.cs
@@ -13,6 +13,33 @@ namespace RSSReader
 {
     public class Library
     {
+        private async void load(ItemsControl list, Uri uri)
+        {
+            SyndicationClient client = new SyndicationClient();
+            SyndicationFeed feed = await client.RetrieveFeedAsync(uri);
+            if (feed != null)
+            {
+                foreach(SyndicationItem item in feed.Items)
+                {
+                    list.Items.Add(item);
+                }
+            }
+        }
 
+        public void Go(ref ItemsControl list, string value, KeyRoutedEventArgs arg)
+        {
+            if (arg.Key == Windows.System.VirtualKey.Enter)
+            {
+                try
+                {
+                    load(list, new Uri(value));
+                }
+                catch
+                {
+
+                }
+                list.Focus(FocusState.Keyboard);
+            }
+        }
     }
 }
