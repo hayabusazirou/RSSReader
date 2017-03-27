fffe20706c4b0cb7d001a6919c78e73ed2ade027
yamaguchiICT <yamaguchi@ict.ehime-u.ac.jp>
2017-03-27 09:32:56 +0900

    create Library.cs

---
+++ RSSReader/Library.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.Web.Syndication;
+
+namespace RSSReader
+{
+    public class Library
+    {
+
+    }
+}
